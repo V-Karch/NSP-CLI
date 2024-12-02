20a058d3973e58331995700fcf45dde9da50f34c
V-Karch <luna.karch05@gmail.com>
2024-12-02 13:15:57 -0500

    src/main.rs: added check for no arguments provided

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
 fn main() {
-    let args: Vec<String> = std::env::args().skip(1).collect();
+    let args: Vec<String> = std::env::args()
+        .skip(1)
+        .collect();
+
+    if args.len() == 0 {
+        println!("No arguments supplied"); // Add --help here
+        return; // Leave function early
+    }
+
     println!("{:?}", args);
 }
