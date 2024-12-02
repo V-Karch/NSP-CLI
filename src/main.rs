5daeeff02be38d867efcbe73fc2ff3cb7bf9469f
V-Karch <luna.karch05@gmail.com>
2024-12-02 13:51:48 -0500

    src: added --list command line argument for searching for nsp and xci files

--- src/main.rs
+++ src/main.rs
@@ -10,5 +10,13 @@ fn main() {
         return; // Exit program
     } // No arguments were supplied
 
+    if args[0] == "--list"{
+        if args.len() == 2 {
+            utils::list_possible_nsp_given_path(&args[1]);
+        } else {
+            utils::list_possible_nsp_given_path(&std::env::current_dir().unwrap().display().to_string());
+        }
+    }
+
     println!("{:?}", args); // Display collected arguments for debug purposes
 }
--- src/utils.rs
+++ src/utils.rs
@@ -7,3 +7,21 @@ pub fn display_help_message() {
         \nNote: all <path> will default to current working directory if none is supplied."
     );
 }
+
+pub fn list_possible_nsp_given_path(string_path: &str) {
+    let paths = std::fs::read_dir(string_path).unwrap();
+    let mut found_count: usize = 0;
+
+
+    for raw_path in paths {
+        let relative_path = raw_path.unwrap().path().display().to_string();
+        if relative_path.ends_with(".nsp") || relative_path.ends_with(".xci") {
+            println!("{}", relative_path);
+            found_count += 1;
+        }
+    }
+
+    if found_count == 0 {
+        println!("No nsp files found in directory `{}`", string_path);
+    } // Say something if no files are found
+}
