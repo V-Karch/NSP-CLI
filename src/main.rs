f87b0e38619ee64f1fc7562af86fd7fab07b6422
V-Karch <luna.karch05@gmail.com>
2024-12-02 13:30:48 -0500

    src/main.rs: added a help message for when user does not supply any arguments

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,14 @@ fn main() {
         .collect();
 
     if args.len() == 0 {
-        println!("No arguments supplied"); // Add --help here
-        return; // Leave function early
+        println!(
+            "Usage:\n./nsp-cli --help\n\
+            \t--list <path>: Lists all nsp files or part files in the specified directory\n\
+            \t--split <path>: Splits an nsp file into fat32-compatible chunks given the path to it\n\
+            \t--combine <path>: Combines multiple nsp file parts into one singular resulting nsp file given a path to a folder containing nsp parts\n\
+            \nNote: all <path> will default to current working directory if none is supplied."
+        );
+        return; // Exit program
     }
 
     println!("{:?}", args);
